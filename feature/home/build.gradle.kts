c4730b819f5d001a27b60973fcde9529954e8a26
msmoradi <mr.ms.moradi@gmail.com>
2023-03-08 01:22:07 +0330

    Add missed implementation

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -10,5 +10,7 @@ android {
 dependencies {
     implementation(project(":core:designsystem"))
     implementation(project(":core:model"))
+    implementation(project(":core:domain"))
+    implementation(project(":core:common"))
     implementation(libs.androidx.compose.material3.windowSizeClass)
 }
