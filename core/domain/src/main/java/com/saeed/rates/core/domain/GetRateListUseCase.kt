c5b5847aea706357eba79d258022d6651933ec67
msmoradi <mr.ms.moradi@gmail.com>
2023-03-08 01:20:14 +0330

    Add get rateList useCase

---
+++ core/domain/src/main/java/com/saeed/rates/core/domain/GetRateListUseCase.kt
@@ -0,0 +1,14 @@
+package com.saeed.rates.core.domain
+
+import com.saeed.rates.core.data.repository.RatesRepository
+import com.saeed.rates.core.model.RateModel
+import javax.inject.Inject
+
+class GetRateListUseCase @Inject constructor(
+    private val ratesRepository: RatesRepository,
+) {
+
+    suspend operator fun invoke(): List<RateModel> {
+        return ratesRepository.getRateList().shuffled()
+    }
+}
