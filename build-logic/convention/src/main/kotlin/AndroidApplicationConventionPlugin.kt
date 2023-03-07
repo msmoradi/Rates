2f082790fe0eb9b53582db1de556f3ba94db5062
msmoradi <mr.ms.moradi@gmail.com>
2023-03-07 16:14:37 +0330

    Add conventions

---
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,29 @@
+import com.android.build.api.variant.ApplicationAndroidComponentsExtension
+import com.android.build.api.dsl.ApplicationExtension
+import com.saeed.rates.configureFlavors
+import com.saeed.rates.configureKotlinAndroid
+import com.saeed.rates.configurePrintApksTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            extensions.configure<ApplicationExtension> {
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = 33
+                configureFlavors(this)
+            }
+            extensions.configure<ApplicationAndroidComponentsExtension> {
+                configurePrintApksTask(this)
+            }
+        }
+    }
+
+}
