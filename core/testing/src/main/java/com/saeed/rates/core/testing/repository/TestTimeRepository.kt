058dd7afcf79d912debb39b901ea587c1ed2a03c
msmoradi <mr.ms.moradi@gmail.com>
2023-03-10 03:03:23 +0330

    Add TestTimeRepository

---
+++ core/testing/src/main/java/com/saeed/rates/core/testing/repository/TestTimeRepository.kt
@@ -0,0 +1,11 @@
+package com.saeed.rates.core.testing.repository
+
+import com.saeed.rates.core.data.repository.TimeRepository
+import com.saeed.rates.core.testing.data.timeTestData
+
+class TestTimeRepository : TimeRepository {
+
+    override fun getCurrentTimeString(): String {
+        return timeTestData
+    }
+}
