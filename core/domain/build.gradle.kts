65d7efbcfe51dd40a2ac208b0f32e8f6c1c22cf3
msmoradi <mr.ms.moradi@gmail.com>
2023-03-10 03:06:29 +0330

    Add testImplementation on build gradle

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
 
     implementation(project(":core:data"))
     implementation(project(":core:model"))
+    testImplementation(project(":core:testing"))
 
     implementation(libs.kotlinx.coroutines.android)
 
